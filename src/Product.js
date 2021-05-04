3ef78b5421a174d8e901dfdfb3cdc78822b1523d
Simon Andersen <simo414u@stud.kea.dk>
2021-05-04 18:33:02 +0200

    Added soldOut functionality

--- src/Product.js
+++ src/Product.js
@@ -1,15 +1,15 @@
 export default function Product(props) {
-    console.log(props);
+    console.log(props.soldOut);
     // const clicked = (btn) => {
     //     console.log(`You selected the ${props.displayname}`);
     // }
     return (
-        <div className="Product">
+        <article className={props.soldOut ? "Product soldOut" : "Product"}>
             <img src="https://picsum.photos/300/300" alt={props.displayname}></img>
             <h2>{props.displayname}</h2>
             <div className="ProductDescription">{props.description}</div>
             <div className="Price">{props.price} DOLLARS</div>
-        </div>
+        </article>
     )
 }
 //<button className="more_info" onClick={clicked}></button>
